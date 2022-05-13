87f188c0bf37277b126b7cfca0bc5b064569e306
Facio <80366331+josemfacio@users.noreply.github.com>
2022-05-13 01:30:14 -0400

    Actualización de Rutas
    Se modifico las rutas

--- routers/index.js
+++ routers/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 
 const student = require("./student");
-const teacher = require("./teacher");
-const course = require("./course");
+const loan = require("./loan");
 const editorial = require("./editorial");
+const categories = require("./categories");
 
 function routerApi(app) {
   const router = express.Router();
 
   app.use("/univalle/v1", router);
 
-  router.use(`/student`, student);
-  router.use(`/teacher`, teacher);
-  router.use(`/course`, course);
-  router.use(`/editorial`, editorial);
+  router.use("/editorial", editorial);
+  router.use("/student", student);
+  router.use("/loan", loan);
+  router.use("/categories", categories);
 }
 
 module.exports = routerApi;
