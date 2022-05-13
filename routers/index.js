46da16b8f171bb3ea83ae63a678e9353d8a86c24
Facio <80366331+josemfacio@users.noreply.github.com>
2022-05-13 02:15:56 -0400

    Cambio de nombre
    Cambie de nombre a 2 archivos y añadi su ruta del luis a index en rutas

--- routers/index.js
+++ routers/index.js
@@ -4,12 +4,14 @@ const student = require("./student");
 const loan = require("./loan");
 const editorial = require("./editorial");
 const categories = require("./categories");
+const reserve = require("./reserve");
 
 function routerApi(app) {
   const router = express.Router();
 
   app.use("/univalle/v1", router);
 
+  router.use("/reserve", reserve);
   router.use("/editorial", editorial);
   router.use("/student", student);
   router.use("/loan", loan);
